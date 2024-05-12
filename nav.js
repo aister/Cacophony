7cfeb7005ae4afff2d3e4f3b8a2797fcdfdb1354
aister <hellodecasino@gmail.com>
2024-05-12 14:08:38 +0700

    Add files via upload

--- nav.js
+++ nav.js
@@ -1,17 +1,30 @@
 const nav = document.querySelector("#nav");
 
 const navList = [
+  ["Home", "./index.html"],
   ["About", "./about.html"],
   ["Contact", "./contact.html"],
   ["FAQ", "./faq.html"]
 ]
 
-nav.innerHTML = `
-      <a id="logo" href="/">
-          <img alt="Cacophony" src="./img/logo.png" />
-      </a>
-`;
-
+let menuContent = "";
 navList.forEach(i => {
-  nav.innerHTML += `<a class="nav" href="${i[1]}">${i[0]}</a>`;
+  menuContent += `<a class="nav" href="${i[1]}">${i[0]}</a>`;
 })
+
+if (typeof setting == "undefined") nav.innerHTML = `<div style="width: 60px"></div>`;
+
+let elem = document.createElement("a");
+elem.id = "logo";
+elem.href = "./index.html";
+elem.innerHTML = `<img alt="Cacophony" src="./img/logo.png" />`;
+nav.appendChild(elem);
+
+elem = document.createElement("div");
+elem.id = "menu";
+elem.innerHTML = `
+  <img src="./img/menu.png">
+  <div id="menuContent">${menuContent}</div>
+`;
+elem.onclick = () => { elem.classList.toggle("active"); }
+nav.appendChild(elem);
